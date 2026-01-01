898ee822740fde4c943804de097d7fd4b81aaa48
rajkumqr <raj.khoth17@gmail.com>
2026-01-01 11:26:17 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+addition = a+b
+Subtraction = a-b
+multiplication = a*b
+division = a/b
+print(f"addition of {a} and {b} is ={addition}")
+print(f"Subtraction of {a} and {b} is ={Subtraction}")
+print(f"multiplication of {a} and {b} is ={multiplication}")
+print(f"division of {a} and {b} is ={division}")
+

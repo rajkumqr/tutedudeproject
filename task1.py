ccee4f9ded4e68c54d07227e09d4d086e86da4a3
rajkumqr <raj.khoth17@gmail.com>
2026-01-01 11:35:39 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,12 @@
-a=int(input("enter first number: "))
-b=int(input("enter second number: "))
-addition = a+b
-Subtraction = a-b
-multiplication = a*b
-division = a/b
+a=int(input("enter first number: "))   '''input first number'''
+b=int(input("enter second number: ")) '''input second number'''
+addition = a+b '''add  of given number'''
+Subtraction = a-b ''' subtraction of given number'''
+multiplication = a*b '''multiplication of given number'''
+division = a/b           '''division of  given number'''
 print(f"addition of {a} and {b} is ={addition}")
 print(f"Subtraction of {a} and {b} is ={Subtraction}")
 print(f"multiplication of {a} and {b} is ={multiplication}")
 print(f"division of {a} and {b} is ={division}")
+
 

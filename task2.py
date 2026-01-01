068a92c13d96022c214f0e7b6365dbd5c9f39cfc
rajkumqr <raj.khoth17@gmail.com>
2026-01-01 11:47:33 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,5 @@
+#input your first name
+first_name=input("Enter your first name: ")
+#input your last name
+last_name=input("Enter your last name: ")
+print(f"hello {first_name} {last_name} ! welcome to python program")
